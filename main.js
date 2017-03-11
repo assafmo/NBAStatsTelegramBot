50de4bd8bde5337bf55b228148c9f9322162b7e8
assafmo <assaf.morami@gmail.com>
2017-03-11 21:28:51 +0200

    fix bug track -> follow

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const Twitter = require('twitter');
 
 const config = require(path.join(__dirname, 'config.json'));
 
-const TwitterAccountIDToFollow = config.twitter.account_id_to_follow;
+const twitterAccountIDToFollow = config.twitter.account_id_to_follow;
 const telegramBotUrl = `https://api.telegram.org/bot${config.telegram.bot_key}`;
 const telegramChatID = config.telegram.chat_id;
 
@@ -23,13 +23,13 @@ const keywords = Promise.all(require('fs')
         .reduce((x, y) => x.concat(y), [])
         .map(keyword => keyword.toLowerCase()));
 
-twitterClient.stream('statuses/filter', { track: 'nba' }, function (stream) {
+twitterClient.stream('statuses/filter', { follow: twitterAccountIDToFollow }, function (stream) {
     stream.on('data', function (tweet) {
         if (tweet.lang != 'en')
             return;
         if (!tweet.user)
             return;
-        if (tweet.user.id != TwitterAccountIDToFollow)
+        if (tweet.user.id != twitterAccountIDToFollow)
             return;
 
         keywords.then(keywords => {
