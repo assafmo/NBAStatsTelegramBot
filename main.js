915cc0f5d5e4416efc8d403cdb8526740cb9608f
assafmo <assaf.morami@gmail.com>
2017-03-12 13:36:17 +0200

    refactor, if debug=true then just track "nba"

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const path = require('path');
 const request = require('request');
 const Twitter = require('twitter');
 
-const config = require(path.join(__dirname, 'config.json'));
+const debug = true;
+const config = require(path.join(__dirname, `config${debug ? '_test' : ''}.json`));
 
 const twitterAccountIDToFollow = config.twitter.account_id_to_follow;
 const telegramBotUrl = `https://api.telegram.org/bot${config.telegram.bot_key}`;
@@ -15,42 +16,68 @@ const twitterClient = new Twitter({
     access_token_secret: config.twitter.access_token_secret
 });
 
-const keywords = Promise.all(require('fs')
-    .readdirSync(path.join(__dirname, '/keywords'))
-    .map(f => require(path.join(__dirname, '/keywords', f)))
-)
-    .then(keywordsArrays => keywordsArrays
-        .reduce((x, y) => x.concat(y), [])
-        .map(keyword => keyword.toLowerCase()));
+const keywordsFiles = require('fs').readdirSync(path.join(__dirname, '/keywords'));
+const keywordsPromieses = keywordsFiles.map(f => require(path.join(__dirname, '/keywords', f)));
+const keywordsPromise = Promise.all(keywordsPromieses)
+    .then(keywordsArrays =>
+        keywordsArrays
+            .reduce((x, y) => x.concat(y), [])
+            .map(keyword => keyword.toLowerCase()));
 
-twitterClient.stream('statuses/filter', { follow: twitterAccountIDToFollow }, function (stream) {
+const followData = debug ?
+    { track: 'nba' } :
+    { follow: twitterAccountIDToFollow };
+
+twitterClient.stream('statuses/filter', followData, function (stream) {
     stream.on('data', function (tweet) {
-        if (tweet.lang != 'en')
-            return;
-        if (!tweet.user)
-            return;
-        if (tweet.user.id != twitterAccountIDToFollow)
-            return;
+        if (!debug) {
+            if (!tweet.user)
+                return;
+            if (tweet.user.id != twitterAccountIDToFollow)
+                return;
+        }
 
-        keywords.then(keywords => {
-            const lowerTweetText = tweet.text.toLowerCase().replace(/\n/g, '');
-            for (let keyword of keywords) {
-                if (lowerTweetText.includes(keyword)) {
-                    const photo = tweet.entities &&
-                        tweet.entities.media &&
-                        tweet.entities.media.length &&
-                        tweet.entities.media.filter(m => m.type == 'photo')[0];
-                    const url = `${telegramBotUrl}/${photo ? 'sendPhoto' : 'sendMessage'}`;
-                    const form = Object.assign(
-                        { chat_id: telegramChatID, disable_web_page_preview: true },
-                        photo ? { photo: photo.media_url_https || photo.media_url, caption: tweet.text } : { text: tweet.text }
-                    );
-
-                    request.post({ url: url, form: form });
-                    console.log(tweet.text);
-                    return;
+        keywordsPromise.then(keywordsLower => {
+            const tweetTextLower = tweet.text.toLowerCase().replace(/\n/g, '');
+            let isNbaRelated = true;
+            for (let keyword of keywordsLower) {
+                if (tweetTextLower.includes(keyword)) {
+                    isNbaRelated = true;
+                    break;
                 }
             }
+
+            if (!isNbaRelated)
+                return;
+
+            const photo = tweet.entities &&
+                tweet.entities.media &&
+                tweet.entities.media.length &&
+                tweet.entities.media.filter(m => m.type == 'photo')[0];
+
+            let telegramMessageUrl, telegranMessageData;
+            if (photo) {
+                telegramMessageUrl = `${telegramBotUrl}/sendPhoto`;
+                telegranMessageData = {
+                    chat_id: telegramChatID,
+                    photo: photo.display_url || photo.media_url_https || photo.media_url,
+                    caption: tweet.text
+                };
+            }
+            else {
+                telegramMessageUrl = `${telegramBotUrl}/sendMessage`;
+                telegranMessageData = {
+                    chat_id: telegramChatID,
+                    text: tweet.text,
+                    disable_web_page_preview: true
+                };
+            }
+
+            request.post({
+                url: telegramMessageUrl,
+                form: telegranMessageData
+            });
+            console.log(tweet.text);
         });
     });
 
@@ -59,3 +86,4 @@ twitterClient.stream('statuses/filter', { follow: twitterAccountIDToFollow }, fu
         process.exit(1);
     });
 });
+
