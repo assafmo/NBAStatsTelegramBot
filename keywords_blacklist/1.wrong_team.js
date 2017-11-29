29128b38a06067a307dd210d25a3e26901fdb05f
assafmo <assaf.morami@gmail.com>
2017-11-29 17:17:15 +0200

    kings rankings blacklist

--- keywords_blacklist/1.wrong_team.js
+++ keywords_blacklist/1.wrong_team.js
@@ -1,4 +1,5 @@
 module.exports = Promise.resolve([
     { is_accepted_because: "Hawks", blacklist: "Black Hawks", },
-    { is_accepted_because: "kings", blacklist: "Vikings", }
+    { is_accepted_because: "kings", blacklist: "Vikings", },
+    { is_accepted_because: "kings", blacklist: "Rankings", }
 ]);
