11683d8b211b7195052e734df49ddfd7b6736848
assafmo <assaf.morami@gmail.com>
2017-12-29 13:14:42 +0200

    Blocklist Hartford Hawks

--- main.js
+++ main.js
@@ -249,7 +249,8 @@ const blacklistPromise = Promise.resolve([
   { is_accepted_because: "triple double", blacklist: "Division I" },
   { is_accepted_because: "Blazers", blacklist: "UAB" },
   { is_accepted_because: "Bobcats", blacklist: "Ohio" },
-  { is_accepted_because: "Bulls", blacklist: "South Florida" }
+  { is_accepted_because: "Bulls", blacklist: "South Florida" },
+  { is_accepted_because: "Hawks", blacklist: "Hartford" }
 ]);
 
 function getPhotos(tweet) {
